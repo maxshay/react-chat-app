9abb737842efef9098ca470a28a30087f0f352e4
maxshay <maksimshaynyuk@gmail.com>
2020-02-05 12:51:37 -0800

    placeholder

--- src/components/join/Join.js
+++ src/components/join/Join.js
@@ -12,8 +12,8 @@ const Join = () => {
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
         <h1 className="heading">Join</h1>
-        <div><input type="text" placeholder="" className="joinInput" onChange={(e) => setName(e.target.value)}/></div>
-        <div><input type="text" placeholder="" className="joinInput mt-20" onChange={(e) => setRoom(e.target.value)}/></div>
+        <div><input type="text" placeholder="Name" className="joinInput" onChange={(e) => setName(e.target.value)}/></div>
+        <div><input type="text" placeholder="Room" className="joinInput mt-20" onChange={(e) => setRoom(e.target.value)}/></div>
         <Link onClick={(e) => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
           <button className="button mt-20" type="submit">Sign in</button>
         </Link>
